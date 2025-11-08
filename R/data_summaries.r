abce104ff049cefb6e6d10cd999f44f72ea3b943
ccsarapas <ccsarapas@chestnut.org>
2025-11-08 17:26:10 -0600

    implement write categorical summary

--- R/data_summaries.r
+++ R/data_summaries.r
@@ -9,7 +9,7 @@ cb_summarize_numeric <- function(cb) {
 
   res <- summary_table(
     data,
-    `valid n` = n_valid, `valid %` = pct_valid,
+    `Valid n` = n_valid, valid_pct = pct_valid,
     mean, SD = sd,
     median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
     skew = moments::skewness, kurt = moments::kurtosis,
@@ -26,22 +26,22 @@ cb_count <- function(data,
                      detail_missing = FALSE,
                      detail_na_label = "NA") {
   levels <- if (prefixed) "prefixed" else "labels"
-  data <- dplyr::rename(data, Value = {{ var }})
-  if (!detail_missing) data$Value <- labelled::user_na_to_na(data$Value)
+  data <- dplyr::rename(data, value = {{ var }})
+  if (!detail_missing) data$value <- labelled::user_na_to_na(data$value)
   na_label <- if (detail_missing) detail_na_label else "(Missing)"
-  valid_labs <- val_labels_valid(data$Value)
+  valid_labs <- val_labels_valid(data$value)
   out <- data |>
-    dplyr::count(Value) |>
-    tidyr::complete(Value = valid_labs, fill = list(n = 0)) |>
-    dplyr::mutate(Variable = as.character(rlang::ensym(var)), .before = Value) |>
+    dplyr::count(value) |>
+    tidyr::complete(value = valid_labs, fill = list(n = 0)) |>
+    dplyr::mutate(name = as.character(rlang::ensym(var)), .before = value) |>
     dplyr::mutate(
-      missing = is.na(Value),
-      Value = as.character(labelled::to_factor(
-        Value,
+      missing = is.na(value),
+      value = as.character(labelled::to_factor(
+        value,
         levels = levels, user_na_to_na = !detail_missing,
         explicit_tagged_na = detail_missing
       )),
-      Value = tidyr::replace_na(Value, na_label),
+      value = tidyr::replace_na(value, na_label),
       pct_of_all = n / sum(n),
       pct_of_valid = ifelse(missing, NA, n / sum(n[!missing]))
     ) |>
@@ -49,23 +49,23 @@ cb_count <- function(data,
   if (detail_missing) {
     out <- out |>
       dplyr::mutate(
-        pct_valid_miss = ifelse(
+        `valid / missing` = ifelse(
           missing,
           sum(n[missing]) / sum(n),
           sum(n[!missing]) / sum(n)
         ),
-        pct_valid_miss = glue_chr(
+        `valid / missing` = glue_chr(
           "{ifelse(missing, 'Missing', 'Valid')} ",
-          "({sprintf('%.1f%%', pct_valid_miss * 100)})"
+          "({sprintf('%.1f%%', `valid / missing` * 100)})"
         ),
-        .after = Value
+        .before = value
       ) |>
       dplyr::mutate(
         pct_of_missing = ifelse(missing, n / sum(n[missing]), NA),
         .after = pct_of_valid
       )
   }
-  out
+  dplyr::select(out, !missing)
 }
 
 cb_count_multiple <- function(data, 
--- R/write_codebook.r
+++ R/write_codebook.r
@@ -3,37 +3,125 @@ cb_write_sheet <- function(wb,
                            data, 
                            sheet_name,
                            header = NULL,
-                           format_names = TRUE,
                            cols_pct = NULL,
-                           cols_int = NULL) {
-  cols <- seq_along(data)
+                           cols_int = NULL,
+                           variable_bands = TRUE,
+                           variable_borders = FALSE,
+                           sub_borders = NULL) {
+  cols <- list(
+    all = seq_along(data),
+    num = which(sapply(data, is.numeric)),
+    pct = which(names(data) %in% untidyselect(data, {{cols_pct}})),
+    int = which(names(data) %in% untidyselect(data, {{cols_int}}))
+  )
   nrows <- nrow(data)
   rows <- tibble::lst(
     dat_start = length(header) + 1,
     header = seq_along(header),
     dat = seq_len(nrows) + dat_start,
-    all = seq_len(nrows + dat_start),
-    banded = dat[seq(1, nrows, by = 2)]
+    all = seq_len(nrows + dat_start)
   )
-  num_fmts <- list(
-    num = list(cols = which(sapply(data, is.numeric)), fmt = "0.00"),
-    pct = list(cols = which(names(data) %in% untidyselect(data, {{cols_pct}})),
-               fmt = "0.0%"),
-    int = list(cols = which(names(data) %in% untidyselect(data, {{cols_int}})),
-               fmt = "0")
-  )
+  num_fmts <- list(num = "0.00", pct = "0.0%", int = "0")
   
-  # Add worksheet and set base styles
-  wb <- openxlsx2::wb_add_worksheet(wb, sheet_name) |> 
+  ## prep data for writing
+  data <- data |>
+    dplyr::mutate(
+      dplyr::across(tidyselect::where(is.character), \(x) tidyr::replace_na(x, ""))
+    )
+  index_repeats <- function(data, var) {
+    data |>
+      dplyr::mutate(idx = {{ var }} != dplyr::lag({{ var }}, default = "")) |>
+      dplyr::pull(idx)
+  }
+  block_bottoms <- function(var_change, rows) {
+    c(rows$dat[which(var_change)][-1] - 1, max(rows$dat))
+  }
+  if (variable_bands || variable_borders || !missing(sub_borders)) {
+    var_change <- index_repeats(data, Name)
+    if (variable_bands) {
+      rows$bands <- rows$dat[
+        dplyr::near(dplyr::consecutive_id(data$Name) %% 2, 1)
+      ]
+    }
+    if (variable_borders) {
+      rows$var_borders <- block_bottoms(var_change, rows)
+      cols$var_borders <- cols$all
+    }
+    if (variable_bands || variable_borders) data$Name[!var_change] <- ""
+    if (!missing(sub_borders)) {
+      sub_var_chr <- as.character(rlang::ensym(sub_borders))
+      var_change_sub <- index_repeats(data, {{ sub_borders }})
+      rows$sub_borders <- block_bottoms(var_change_sub, rows)
+      cols$sub_borders <- seq(match(sub_var_chr, names(data)), max(cols$all))
+      data <- data |>
+        dplyr::mutate("{sub_var_chr}" := ifelse(
+          var_change_sub | var_change, 
+          {{ sub_borders }}, 
+          ""
+        ))
+    }
+  }
+  
+  ## Add worksheet and apply styles
+  wb <- wb |>
+    openxlsx2::wb_add_worksheet(sheet_name) |>
     openxlsx2::wb_add_font(
-      sheet_name, openxlsx2::wb_dims(rows$all, cols), "Aptos Narrow"
+      sheet_name, openxlsx2::wb_dims(rows$all, cols$all), "Aptos Narrow"
+    ) |>
+    openxlsx2::wb_add_font(
+      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all),
+      italic = TRUE
     ) |>
     openxlsx2::wb_add_fill(
-      sheet_name, openxlsx2::wb_dims(rows$all, cols),
+      sheet_name, openxlsx2::wb_dims(rows$all, cols$all),
       openxlsx2::wb_color("white")
+    ) |>
+    openxlsx2::wb_add_border(
+      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all), 
+      top_color = openxlsx2::wb_color(hex = "808080"), 
+      bottom_color = openxlsx2::wb_color("black"), bottom_border = "double",
+      left_border = NULL, right_border = NULL
     )
+  if (variable_bands) {
+    wb <- wb |>
+      openxlsx2::wb_add_fill(
+        sheet_name, openxlsx2::wb_dims(rows$bands, cols$all),
+        openxlsx2::wb_color(hex = "EEEEEE")
+      )
+  }
+  for (nm in intersect(names(cols), c("var_borders", "sub_borders"))) {
+    for (r in rows[[nm]]) {
+      wb <- wb |>
+        openxlsx2::wb_add_border(
+          sheet_name, openxlsx2::wb_dims(r, cols[[nm]]),
+          bottom_color = openxlsx2::wb_color("black"),
+          top_border = NULL, left_border = NULL, right_border = NULL
+        )
+    }
+  }
+  if (length(cols$num)) {
+    wb <- wb |>
+      openxlsx2::wb_add_cell_style(
+        sheet_name, 
+        openxlsx2::wb_dims(c(rows$dat_start, rows$dat), cols$num),
+        horizontal = "right"
+      )
+    for (nm in names(num_fmts)) {
+      wb <- wb |>
+        openxlsx2::wb_add_numfmt(
+          sheet_name, openxlsx2::wb_dims(rows$dat, cols[[nm]]), 
+          numfmt = num_fmts[[nm]]
+        )
+    }
+  }
 
-  # Write header
+  ## Write data
+  wb <- wb |>
+    openxlsx2::wb_add_data(
+      sheet_name, data,
+      start_row = rows$dat_start, start_col = 1,
+      na.strings = "-"
+    )
   if (length(header)) {
     wb <- wb |>
       openxlsx2::wb_add_data(
@@ -43,64 +131,23 @@ cb_write_sheet <- function(wb,
         sheet_name, openxlsx2::wb_dims(rows$header, 1), bold = TRUE
       )
   }
-  
-  # Format column names
-  if (format_names) {
-    names(data) <- stringr::str_to_title(
-      stringr::str_replace_all(names(data), "_", " ")
-    )
-  }
-  
-  # Write data and apply styles
-  wb <- wb |>
-    openxlsx2::wb_add_data(
-      sheet_name, data, start_row = rows$dat_start, start_col = 1, 
-      na.strings = ""
-    ) |>
-    # Row banding
-    openxlsx2::wb_add_fill(
-      sheet_name, openxlsx2::wb_dims(rows$banded, cols),
-      openxlsx2::wb_color(hex = "EEEEEE")
-    ) |>
-    # Style variable names
-    openxlsx2::wb_add_font(
-      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols), italic = TRUE
-    ) |>
-    openxlsx2::wb_add_border(
-      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols), 
-      top_color = openxlsx2::wb_color(hex = "808080"), top_border = "thin",
-      bottom_color = openxlsx2::wb_color("black"), bottom_border = "double",
-      left_border = "none", right_border = "none"
-    )
-  
-  # Format and style numeric columns
-  if (length(num_fmts$num$cols)) {
-    wb <- wb |>
-      openxlsx2::wb_add_cell_style(
-        sheet_name, openxlsx2::wb_dims(rows$dat_start, num_fmts$num$cols),
-        horizontal = "right"
-      )
-    for (fmt in num_fmts) {
-      wb <- wb |>
-        openxlsx2::wb_add_numfmt(
-          sheet_name, openxlsx2::wb_dims(rows$dat, fmt$cols), numfmt = fmt$fmt
-        )
-    }
-  }
   # Set column widths and freeze panes
   wb <- wb |>
-    openxlsx2::wb_set_col_widths(sheet_name, cols, widths = "auto") |>
+    openxlsx2::wb_set_col_widths(sheet_name, cols$all, widths = "auto") |>
     openxlsx2::wb_freeze_pane(
       sheet_name, first_active_row = rows$dat_start + 1,  first_active_col = 2
     )
   
   wb
 }
+  
+
+
 
 # incl_date = TRUE 
 # incl_dims = TRUE
 # dataset_name = "UGH sus REDCap"
-# file = file.path("..", "Tests", "summary_num2.xlsx")
+# file = file.path("..", "Tests", "summary.xlsx")
 # overwrite = TRUE
 
 # cb_name <- cb_dims <- cb_date <- NULL
@@ -112,17 +159,32 @@ cb_write_sheet <- function(wb,
 # }
 # cb_date <- if (incl_date) glue_chr("Codebook generated {Sys.Date()}")
 # h_dict <- c(dataset_name, cb_dims, cb_date)
-# h_summ <- c(dataset_name, "Numeric variables summary")
+# h_summ_num <- c(dataset_name, "Numeric variables summary")
+# h_summ_cat <- c(dataset_name, "Categorical variables summary")
 
-# openxlsx2::wb_workbook() |> 
-#   cb_write_sheet(codebook_out, "Overview", header = h_dict, cols_pct = missing) |> 
+
+# codebook_out <- cb |>
+#   dplyr::relocate(form, type, .after = name) |>
+#   dplyr::select(!tidyselect::any_of(c("rc_type"))) |>
+#   cb_format_names()
+# summ_num <- cb_summarize_numeric(cb) |>
+#   cb_format_names(!c(`Valid n`, SD, MAD))
+# summ_cat <- cb_summarize_categorical(cb) |>
+#   cb_format_names(!n)
+
+
+# openxlsx2::wb_workbook() |>
+#   cb_write_sheet(codebook_out, "Overview", header = h_dict, cols_pct = Missing) |>
 #   cb_write_sheet(
-#     summ_num, "Summary - Numeric Vars", header = h_summ, format_names = FALSE, 
-#     cols_pct = `valid %`, cols_int = `valid n`
+#     summ_num, "Summary - Numeric Vars",
+#     header = h_summ_num, cols_pct = `Valid %`, cols_int = `Valid n`
+#   ) |>
+#   cb_write_sheet(
+#     summ_cat, "Summary - Categorical Vars",
+#     header = h_summ_cat, cols_pct = tidyselect::starts_with("%"), cols_int = n,
+#     variable_borders = TRUE, sub_borders = `Valid / Missing`
 #   ) |> 
 #   openxlsx2::wb_save(file, overwrite = overwrite)
 # # invisible(file)
 
 # lighthouse::open_file(file)
-
-
