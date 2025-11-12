f867c69d475ebecbf5d8512a9bbaa9e0299b80e8
ccsarapas <ccsarapas@chestnut.org>
2025-11-12 17:15:08 -0600

    start implement decked groups

--- R/data_summaries.r
+++ R/data_summaries.r
@@ -1,67 +1,130 @@
+# think about whether / how to handle missing values in `group_cols`
 
 ## methods for dates, datetimes, etc?
+# cb_summarize_numeric_old <- function(cb, group_cols = NULL) {
+#   out <- cb |>
+#     dplyr::filter(type %in% c("numeric", "integer")) |>
+#     dplyr::select(name, label)
+#   nms_num <- out$name
+#   data <- attr(cb, "data_zapped")
+#   # should be able to just pass `group_cols` directly, but due to bug in
+#   # `summary_table()`, have to do this for now
+#   # (see https://github.com/ccsarapas/lighthouse/issues/41)
+#   if (missing(group_cols)) {
+#     res <- summary_table(
+#       data,
+#       `Valid n` = n_valid, valid_pct = pct_valid,
+#       mean, SD = sd,
+#       median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
+#       skew = moments::skewness, kurt = moments::kurtosis,
+#       na.rm = TRUE,
+#       .vars = tidyselect::all_of(nms_num)
+#     )
+#   } else {
+#     group_chr <- untidyselect(data, {{ group_cols }})
+#     group_glue <- paste0("{", c(group_chr, ".value"), "}", collapse = "|DECK|")
+#     res <- summary_table(
+#       data,
+#       `Valid n` = n_valid, valid_pct = pct_valid,
+#       mean, SD = sd,
+#       median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
+#       skew = moments::skewness, kurt = moments::kurtosis,
+#       na.rm = TRUE,
+#       .vars = tidyselect::all_of(nms_num),
+#       .cols_group_by = {{ group_cols }},
+#       .cols_group_glue = group_glue
+#     )
+#   }
+#   dplyr::left_join(out, res, dplyr::join_by(name == Variable)) |>
+#     nan_to_na()
+# }
+
 cb_summarize_numeric <- function(cb, group_cols = NULL) {
   out <- cb |>
     dplyr::filter(type %in% c("numeric", "integer")) |>
     dplyr::select(name, label)
   nms_num <- out$name
   data <- attr(cb, "data_zapped")
-
-  # should be able to just pass `group_cols` directly, but due to bug in
-  # `summary_table()`, have to do this for now
-  # (see https://github.com/ccsarapas/lighthouse/issues/41)
-  if (is.null(group_cols)) {
-    res <- summary_table(
-      data,
-      `Valid n` = n_valid, valid_pct = pct_valid,
-      mean, SD = sd,
-      median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
-      skew = moments::skewness, kurt = moments::kurtosis,
-      na.rm = TRUE,
-      .vars = tidyselect::all_of(nms_num)
-    )
-  } else {
-    res <- summary_table(
-      data,
-      `Valid n` = n_valid, valid_pct = pct_valid,
-      mean, SD = sd,
-      median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
-      skew = moments::skewness, kurt = moments::kurtosis,
-      na.rm = TRUE,
-      .vars = tidyselect::all_of(nms_num),
-      .cols_group_by = {{ group_cols }}
-    )
-  }
-  dplyr::left_join(out, res, dplyr::join_by(name == Variable)) |>
+  res <- summary_table(
+    data,
+    `Valid n` = n_valid, valid_pct = pct_valid,
+    mean, SD = sd,
+    median, MAD = mad, min = min_if_any, max = max_if_any, range = spread_if_any,
+    skew = moments::skewness, kurt = moments::kurtosis,
+    na.rm = TRUE,
+    .vars = tidyselect::all_of(nms_num),
+    .rows_group_by = {{ group_cols }}
+  )
+  
+  out <- out |> 
+    dplyr::left_join(res, dplyr::join_by(name == Variable)) |>
     nan_to_na()
 }
 
+
+
 cb_count <- function(data,
                      var,
+                     .by = NULL,
                      prefixed = TRUE,
                      detail_missing = FALSE,
                      detail_na_label = "NA") {
+  to_fct_chr <- function(x, levels, detail_missing, na_label) {
+    x |> 
+      labelled::to_factor(
+        levels = levels, user_na_to_na = !detail_missing,
+        explicit_tagged_na = detail_missing
+      ) |>
+      as.character() |> 
+      tidyr::replace_na(na_label)
+  }
+  # if (detail_missing && !missing(.by)) {
+  #   cli::cli_abort(c(
+  #     "Detailed missing value information is not currently supported for multiple groups.",
+  #     "i" = "{.code detail_missing} cannot be {.code TRUE} when {.code .by} is specified."      
+  #   ))
+  # }
   levels <- if (prefixed) "prefixed" else "labels"
   data <- dplyr::rename(data, value = {{ var }})
   if (!detail_missing) data$value <- labelled::user_na_to_na(data$value)
   na_label <- if (detail_missing) detail_na_label else "(Missing)"
   valid_labs <- val_labels_valid(data$value)
   out <- data |>
+    dplyr::group_by(dplyr::pick({{ .by }})) |>
     dplyr::count(value) |>
     tidyr::complete(value = valid_labs, fill = list(n = 0)) |>
-    dplyr::mutate(name = as.character(rlang::ensym(var)), .before = value) |>
     dplyr::mutate(
       missing = is.na(value),
-      value = as.character(labelled::to_factor(
-        value,
-        levels = levels, user_na_to_na = !detail_missing,
-        explicit_tagged_na = detail_missing
-      )),
-      value = tidyr::replace_na(value, na_label),
       pct_of_all = n / sum(n),
       pct_of_valid = ifelse(missing, NA, n / sum(n[!missing]))
     ) |>
-    dplyr::arrange(missing)
+    dplyr::ungroup() |> 
+    dplyr::mutate(name = as.character(rlang::ensym(var)), .before = value) |>
+    dplyr::mutate(
+      dplyr::across({{ .by }}, \(x) to_fct_chr(
+        x, levels ="labels", detail_missing = detail_missing, 
+        na_label = na_label
+      )),
+      value = to_fct_chr(
+        value, levels = levels, detail_missing = detail_missing, 
+        na_label = na_label
+      )
+    ) |>
+    # dplyr::arrange(missing)
+    dplyr::arrange(dplyr::pick({{ .by }}), missing)
+    # dplyr::mutate(name = as.character(rlang::ensym(var)), .before = value) |>
+    # dplyr::mutate(
+    #   missing = is.na(value),
+    #   value = as.character(labelled::to_factor(
+    #     value,
+    #     levels = levels, user_na_to_na = !detail_missing,
+    #     explicit_tagged_na = detail_missing
+    #   )),
+    #   value = tidyr::replace_na(value, na_label),
+    #   pct_of_all = n / sum(n),
+    #   pct_of_valid = ifelse(missing, NA, n / sum(n[!missing]))
+    # ) |>
+    # dplyr::arrange(missing)
   if (detail_missing) {
     out <- out |>
       dplyr::mutate(
@@ -86,22 +149,24 @@ cb_count <- function(data,
 
 cb_count_multiple <- function(data, 
                               ..., 
+                              .by = NULL,
                               .prefixed = TRUE, 
-                              .detail_missing = FALSE, 
+                              .detail_missing = FALSE,
                               .detail_na_label = "NA",
                               .no_prefix = NULL) {
   dots <- rlang::list2(...)
   .prefixed <- .prefixed & !(as.character(dots) %in% .no_prefix)
   purrr::map2_dfr(dots, .prefixed, \(v, p) {
     cb_count(
-      data, var = {{ v }}, prefixed = p, 
-      detail_missing = .detail_missing, detail_na_label = .detail_na_label
+      data, var = {{ v }}, prefixed = p, detail_missing = .detail_missing, 
+      detail_na_label = .detail_na_label, .by = {{ .by }}
     )
   })
 }
 
 
 cb_summarize_categorical <- function(cb, 
+                                     group_cols = NULL,
                                      prefixed = TRUE, 
                                      detail_missing = TRUE, 
                                      detail_na_label = "NA") {
@@ -111,10 +176,14 @@ cb_summarize_categorical <- function(cb,
   nms_cat <- summary_cat$name
   factors <- attr(cb, "factors")
   data <- attr(cb, "data_labelled")
+  group_chr <- untidyselect(data, {{ group_cols }})
+  vars <- rlang::syms(setdiff(nms_cat, group_chr))
   cb_count_multiple(
-    data, !!!rlang::syms(nms_cat), .prefixed = prefixed, .no_prefix = factors,
-    .detail_missing = detail_missing, .detail_na_label = detail_na_label
+    data, !!!vars, .prefixed = prefixed, .no_prefix = factors,
+    .detail_missing = detail_missing, .detail_na_label = detail_na_label, 
+    .by = {{ group_cols }}
   )
 }
+
 
 
--- R/write_codebook.r
+++ R/write_codebook.r
@@ -1,33 +1,4 @@
-
-cb_write_sheet <- function(wb,
-                           data, 
-                           sheet_name,
-                           header = NULL,
-                           cols_pct = NULL,
-                           cols_int = NULL,
-                           variable_bands = TRUE,
-                           variable_borders = FALSE,
-                           sub_borders = NULL) {
-  cols <- list(
-    all = seq_along(data),
-    num = which(sapply(data, is.numeric)),
-    pct = which(names(data) %in% untidyselect(data, {{cols_pct}})),
-    int = which(names(data) %in% untidyselect(data, {{cols_int}}))
-  )
-  nrows <- nrow(data)
-  rows <- tibble::lst(
-    dat_start = length(header) + 1,
-    header = seq_along(header),
-    dat = seq_len(nrows) + dat_start,
-    all = seq_len(nrows + dat_start)
-  )
-  num_fmts <- list(num = "0.00", pct = "0.0%", int = "0")
-  
-  ## prep data for writing
-  data <- data |>
-    dplyr::mutate(
-      dplyr::across(tidyselect::where(is.character), \(x) tidyr::replace_na(x, ""))
-    )
+row_divisions <- function(wb, sheet_name, data, rows, cols, variable_bands, variable_borders, sub_borders, sub_borders_var) {
   index_repeats <- function(data, var) {
     data |>
       dplyr::mutate(idx = {{ var }} != dplyr::lag({{ var }}, default = "")) |>
@@ -36,7 +7,7 @@ cb_write_sheet <- function(wb,
   block_bottoms <- function(chg_rows, rows) {
     c(rows$dat[which(chg_rows)] - 1, max(rows$dat))
   }
-  if (variable_bands || variable_borders || !missing(sub_borders)) {
+    if (variable_bands || variable_borders || sub_borders) {
     chg_rows <- index_repeats(data, Name)
     if (variable_bands) {
       rows$bands <- rows$dat[
@@ -48,46 +19,25 @@ cb_write_sheet <- function(wb,
       cols$var_borders <- cols$all
     }
     if (variable_bands || variable_borders) data$Name[!chg_rows] <- ""
-    if (!missing(sub_borders)) {
-      sub_var_chr <- as.character(rlang::ensym(sub_borders))
-      chg_rows_sub <- index_repeats(data, {{ sub_borders }}) & !chg_rows
+    if (sub_borders) {
+      sub_var_chr <- as.character(rlang::ensym(sub_borders_var))
+      chg_rows_sub <- index_repeats(data, {{ sub_borders_var }}) & !chg_rows
       rows$sub_borders <- block_bottoms(chg_rows_sub, rows)
       cols$sub_borders <- seq(match(sub_var_chr, names(data)), max(cols$all))
       data <- data |>
         dplyr::mutate("{sub_var_chr}" := ifelse(
           chg_rows_sub | chg_rows, 
-          {{ sub_borders }}, 
+          {{ sub_borders_var }}, 
           ""
         ))
     }
   }
-  
-  ## Add worksheet and apply styles
-  wb <- wb |>
-    openxlsx2::wb_add_worksheet(sheet_name) |>
-    openxlsx2::wb_add_font(
-      sheet_name, openxlsx2::wb_dims(rows$all, cols$all), "Aptos Narrow"
-    ) |>
-    openxlsx2::wb_add_font(
-      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all),
-      italic = TRUE
-    ) |>
-    openxlsx2::wb_add_fill(
-      sheet_name, openxlsx2::wb_dims(rows$all, cols$all),
-      openxlsx2::wb_color("white")
-    ) |>
-    openxlsx2::wb_add_border(
-      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all), 
-      top_color = openxlsx2::wb_color(hex = "808080"), 
-      bottom_color = openxlsx2::wb_color("black"), bottom_border = "double",
-      left_border = NULL, right_border = NULL
-    )
   if (variable_bands) {
-    wb <- wb |>
-      openxlsx2::wb_add_fill(
-        sheet_name, openxlsx2::wb_dims(rows$bands, cols$all),
-        openxlsx2::wb_color(hex = "EEEEEE")
-      )
+  wb <- wb |>
+    openxlsx2::wb_add_fill(
+      sheet_name, openxlsx2::wb_dims(rows$bands, cols$all),
+      openxlsx2::wb_color(hex = "EEEEEE")
+    )
   }
   for (nm in intersect(names(cols), c("var_borders", "sub_borders"))) {
     colors <- list(var_borders = "404040", sub_borders = "808080")
@@ -100,6 +50,141 @@ cb_write_sheet <- function(wb,
         )
     }
   }
+  wb
+}
+
+cb_write_sheet <- function(wb,
+                           data, 
+                           sheet_name,
+                           header = NULL,
+                           cols_pct = NULL,
+                           cols_int = NULL,
+                           group_cols = NULL,
+                           id_cols = NULL,
+                           incl_group_col_names = TRUE,
+                           variable_bands = TRUE,
+                           variable_borders = FALSE,
+                           sub_borders = NULL) {
+  # init sheet
+  wb <- wb |>
+    openxlsx2::wb_add_worksheet(sheet_name)  
+  pct_nms <- untidyselect(data, {{cols_pct}})
+  int_nms <- untidyselect(data, {{ cols_int }})
+  group_var_nms <- NULL
+  decked <- NULL
+  if (!missing(group_cols)) {
+    group_var_nms <- untidyselect(data, {{ group_cols }})
+    id_var_nms <- untidyselect(data, {{ id_cols }})
+    group_lvls <- lapply(
+      setNames(nm = group_var_nms),
+      \(x) as.character(sort(unique(data[[x]])))
+    )
+    stat_var_nms <- setdiff(names(data), c(id_var_nms, group_var_nms))
+    cols_decked <- tidyr::expand_grid(!!!c(group_lvls, list(cols = stat_var_nms)))
+    if (incl_group_col_names) {
+      cols_decked <- cols_decked |>
+        dplyr::mutate(
+          dplyr::across(!cols, \(x) stringr::str_c(dplyr::cur_column(), " = ", x))
+        )
+    }
+    cols_decked <- cols_decked |>
+      dplyr::mutate(
+        dplyr::across(!cols, dplyr::consecutive_id, .names = "{.col}_set")
+      )
+    sets_lowest <- cols_decked[[ncol(cols_decked)]]
+    breaks_at <- which(diff(sets_lowest) == 1)
+    cols_decked <- cols_decked |>
+      add_empty_rows(.after = breaks_at) |>
+      dplyr::mutate(
+        # fill values only if prev and following match
+        dplyr::across(!cols, \(x) {
+          dplyr::coalesce(
+            x,
+            ifelse(dplyr::lag(x) == dplyr::lead(x), dplyr::lag(x), x)
+          )
+        })
+      )
+
+    cols_id_decked <- tibble::tibble(cols = id_var_nms) |>
+      dplyr::bind_rows(cols_decked) |>
+      dplyr::mutate(cols = ifelse(is.na(cols), "_empty_", cols))
+
+    data <- data |>
+      dplyr::mutate(..spacer = NA) |>
+      tidyr::pivot_wider(
+        id_cols = {{ id_cols }},
+        names_from = {{ group_cols }},
+        values_from = tidyselect::all_of(c("..spacer", stat_var_nms)),
+        names_vary = "slowest"
+      ) |>
+      as.data.frame()
+    first_spacer <- grep("^\\.\\.spacer", names(data))[[1]]
+    data[, first_spacer] <- NULL
+
+    names(data) <- cols_id_decked$cols
+
+    decked <- cols_id_decked |>
+      dplyr::select({{ group_cols }}) |>
+      t() |>
+      unname()
+    # decked_sets <- cols_id_decked |>
+    #   dplyr::select(tidyselect::ends_with("_set"))
+  }
+  cols <- list(
+    all = seq_along(data),
+    num = unname(which(sapply(data, is.numeric))),
+    pct = which(names(data) %in% pct_nms),
+    int = which(names(data) %in% int_nms)
+  )
+  nrows <- nrow(data)
+  rows <- tibble::lst(
+    dat_start = length(header) + length(group_var_nms) + 1,
+    decked_start = length(header) + 1,
+    decked = seq_along(group_var_nms) + decked_start,
+    header = seq_along(header),
+    dat = seq_len(nrows) + dat_start,
+    all = seq_len(nrows + dat_start)
+  )
+  names(data) <- make.unique(names(data), sep = "_tmp_")
+  
+  num_fmts <- list(num = "0.00", pct = "0.0%", int = "0")
+  
+  ## prep data for writing
+  data <- data |>
+    dplyr::mutate(
+      dplyr::across(tidyselect::where(is.character), \(x) tidyr::replace_na(x, ""))
+    )
+  
+  ## Apply styles
+  wb <- wb |>
+    openxlsx2::wb_add_font(
+      sheet_name, openxlsx2::wb_dims(rows$all, cols$all), "Aptos Narrow"
+    ) |>
+    openxlsx2::wb_add_font(
+      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all),
+      italic = TRUE
+    ) |>
+    openxlsx2::wb_add_fill(
+      sheet_name, openxlsx2::wb_dims(rows$all, cols$all),
+      openxlsx2::wb_color("white")
+    ) |>
+    openxlsx2::wb_add_border(
+      sheet_name, openxlsx2::wb_dims(max(rows$header), cols$all),
+      bottom_color = openxlsx2::wb_color(hex = "808080"), 
+      top_border = NULL, left_border = NULL, right_border = NULL
+    ) |> 
+    openxlsx2::wb_add_border(
+      sheet_name, openxlsx2::wb_dims(rows$dat_start, cols$all), 
+      # bottom_color = openxlsx2::wb_color("black"), 
+      bottom_border = "double",
+      top_border = NULL, left_border = NULL, right_border = NULL
+    )
+
+  wb <- row_divisions(
+    wb, sheet_name, data, rows, cols, variable_bands, variable_borders, 
+    sub_borders = !missing(sub_borders), sub_borders_var = {{ sub_borders }}
+  )
+
   if (length(cols$num)) {
     wb <- wb |>
       openxlsx2::wb_add_cell_style(
@@ -115,7 +200,9 @@ cb_write_sheet <- function(wb,
         )
     }
   }
-
+  
+  names(data) <- gsub("_tmp_.+", "", names(data))
+  names(data) <- gsub("_empty_", "", names(data))
   ## Write data
   wb <- wb |>
     openxlsx2::wb_add_data(
@@ -123,6 +210,21 @@ cb_write_sheet <- function(wb,
       start_row = rows$dat_start, start_col = 1,
       na.strings = "-"
     )
+  if (!is.null(decked)) {
+    wb <- wb |>
+      openxlsx2::wb_add_data(
+        sheet_name, decked, start_row = rows$decked_start, 
+        start_col = 1, col_names = FALSE, na.strings = ""
+      )
+    for (i in seq_along(rows$decked)) {
+      wb <- openxlsx2::wb_add_border(
+        wb, sheet_name, openxlsx2::wb_dims(
+          i, which(!is.na(decked[i, ])), from_row = rows$decked_start
+        ),
+        top_border = NULL, left_border = NULL, right_border = NULL
+      )
+    }
+  }
   if (length(header)) {
     wb <- wb |>
       openxlsx2::wb_add_data(
@@ -142,4 +244,5 @@ cb_write_sheet <- function(wb,
   wb
 }
   
+
 
